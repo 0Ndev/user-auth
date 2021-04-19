19baa8c9bff0b922128a2e7d6dab467a85314254
Natalia <natalialllepost@gmail.com>
2021-04-19 13:25:57 +0200

    forms and model

--- basic_app/models.py
+++ basic_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfileInfo(models.Model):
+    # create relationship (don't inherit from User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # add any additional attributes
+    portfolio_site = models.URLField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics', blank=True)
+
+    def __str__(self):
+        # build-in attribute of django.contrib.auth.models.User
+        return self.user.username
